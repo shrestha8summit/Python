48ad26ad28b87e7c765d6d8e7b46abbf501e5892
shrestha8summit <summits898@gmail.com>
2024-06-13 05:48:40 +0530

    Final stage

--- weather with GUI.py
+++ weather with GUI.py
@@ -19,7 +19,7 @@ def search():
         opLbl.config(text="City name : "+city.get())
         key = os.getenv("key")
         api_key = key
-        request =  requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.get()}&units=imperial&APPID={"df58e4eca5e8083a7809d50d6b89c0e4"}")
+        request =  requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.get()}&units=imperial&APPID={api_key}")
         
         weather = request.json()['weather'][0]['main']
         temp    = round(request.json()['main']['temp'])
