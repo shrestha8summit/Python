d3840a531c594b57e4e7daa073d40ff70b03c52c
shrestha8summit <summits898@gmail.com>
2024-06-14 08:36:18 +0530

    separated directory acc to level of code

--- tts.py
+++
@@ -1,7 +0,0 @@
-# install pyttsx3 library using pip
-
-import pyttsx3 
-text_speech = pyttsx3.init()
-answer = input("What do you want to convert into speech:\t")
-text_speech.say(answer)
-text_speech.runAndWait()
